ebdf015b13373f20b9ab73553cc0ce2abf7d0a67
mabdulwahid <maryam.abdul-wahid@honeywell.com>
2015-07-07 14:27:52 -0700

    testing

---
+++ MEA_Test/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MEA_Test
+{
+    public partial class Form1 : Form
+    {
+        private void InitializeComboBox()
+        {
+            string[] aircraft = new string[] { "A350", "B787", "Other" };
+            comboBox1.Items.AddRange(aircraft);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeComboBox();
+        }
+
+        
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
